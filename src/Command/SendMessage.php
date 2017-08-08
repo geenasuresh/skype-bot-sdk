7de07815f67a16f6b9a660c3a2a45be24ae2b7ab
geenasuresh <geenasuresh@users.noreply.github.com>
2017-08-08 15:13:59 +1000

    Update SendMessage.php
    slack needs the from field in the reply

--- src/Command/SendMessage.php
+++ src/Command/SendMessage.php
@@ -11,8 +11,14 @@ class SendMessage extends SendActivity  {
      * @param $conversation
      */
     public function __construct($message, $conversation) {
-        $this->activity = new Activity();
+       $this->activity = new Activity();
         $this->activity->setText($message);
+        $from = new \SkypeBot\Entity\Address();
+        //get 'from' from conversation
+        //slack needs this from field in the reply
+        $fromArr = explode(":",$conversation);
+        $from->setId($fromArr[0].":".$fromArr[1]);
+        $this->activity->setFrom($from);        
         $this->conversation = $conversation;
     }
 }
